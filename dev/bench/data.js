0292696134bfa8136fc37890c5954daae79b80a5
github-action-benchmark <github@users.noreply.github.com>
2025-06-25 00:12:29 +0000

    add HTTP Throughput (customBiggerIsBetter) benchmark result for d11cefbcc674bbbc032c422046106329a0b22ff4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750810272967,
+  "lastUpdate": 1750810349771,
   "repoUrl": "https://github.com/devigned/runwasi",
   "entries": {
     "HTTP Throughput": [
@@ -3212,6 +3212,33 @@ window.BENCHMARK_DATA = {
           {
             "name": "HTTP RPS",
             "value": 21897.5056,
+            "unit": "req/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Jiaxiao Zhou",
+            "username": "Mossaka",
+            "email": "duibao55328@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "d11cefbcc674bbbc032c422046106329a0b22ff4",
+          "message": "Merge pull request #856 from dblnz/add-mdbook\n\nInitial work to add a mdbook documentation style",
+          "timestamp": "2025-02-25T20:04:59Z",
+          "url": "https://github.com/devigned/runwasi/commit/d11cefbcc674bbbc032c422046106329a0b22ff4"
+        },
+        "date": 1750810347817,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "HTTP RPS",
+            "value": 20532.5346,
             "unit": "req/s"
           }
         ]
