29559eb183e891f8a193375332d46602e758415e
github-action-benchmark <github@users.noreply.github.com>
2026-02-03 00:15:06 +0000

    add Criterion.rs Benchmark (customSmallerIsBetter) benchmark result for d11cefbcc674bbbc032c422046106329a0b22ff4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1769991787581,
+  "lastUpdate": 1770077706089,
   "repoUrl": "https://github.com/devigned/runwasi",
   "entries": {
     "HTTP Throughput": [
@@ -58074,6 +58074,52 @@ window.BENCHMARK_DATA = {
             "value": 92244473,
             "range": "± 1750743",
             "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Jiaxiao Zhou",
+            "username": "Mossaka",
+            "email": "duibao55328@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "d11cefbcc674bbbc032c422046106329a0b22ff4",
+          "message": "Merge pull request #856 from dblnz/add-mdbook\n\nInitial work to add a mdbook documentation style",
+          "timestamp": "2025-02-25T20:04:59Z",
+          "url": "https://github.com/devigned/runwasi/commit/d11cefbcc674bbbc032c422046106329a0b22ff4"
+        },
+        "date": 1770077695979,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "wamr/memory-usage",
+            "value": 15220,
+            "unit": "kB",
+            "extra": "shim: 12372 kB\nzygote: 2848 kB"
+          },
+          {
+            "name": "wasmedge/memory-usage",
+            "value": 65616,
+            "unit": "kB",
+            "extra": "shim: 53132 kB\nzygote: 12484 kB"
+          },
+          {
+            "name": "wasmer/memory-usage",
+            "value": 18112,
+            "unit": "kB",
+            "extra": "shim: 14816 kB\nzygote: 3296 kB"
+          },
+          {
+            "name": "wasmtime/memory-usage",
+            "value": 18976,
+            "unit": "kB",
+            "extra": "shim: 15844 kB\nzygote: 3132 kB"
           }
         ]
       }
