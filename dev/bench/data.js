46f0d9b69512671da96a87ab397785e5485b041a
github-action-benchmark <github@users.noreply.github.com>
2026-08-15 00:43:31 +0000

    add Criterion.rs Benchmark (customSmallerIsBetter) benchmark result for d11cefbcc674bbbc032c422046106329a0b22ff4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1786669916719,
+  "lastUpdate": 1786754610941,
   "repoUrl": "https://github.com/devigned/runwasi",
   "entries": {
     "HTTP Throughput": [
@@ -84574,6 +84574,52 @@ window.BENCHMARK_DATA = {
             "value": 88391970,
             "range": "± 895512",
             "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Jiaxiao Zhou",
+            "username": "Mossaka",
+            "email": "duibao55328@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "d11cefbcc674bbbc032c422046106329a0b22ff4",
+          "message": "Merge pull request #856 from dblnz/add-mdbook\n\nInitial work to add a mdbook documentation style",
+          "timestamp": "2025-02-25T20:04:59Z",
+          "url": "https://github.com/devigned/runwasi/commit/d11cefbcc674bbbc032c422046106329a0b22ff4"
+        },
+        "date": 1786754599091,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "wamr/memory-usage",
+            "value": 15036,
+            "unit": "kB",
+            "extra": "shim: 12332 kB\nzygote: 2704 kB"
+          },
+          {
+            "name": "wasmedge/memory-usage",
+            "value": 65528,
+            "unit": "kB",
+            "extra": "shim: 53112 kB\nzygote: 12416 kB"
+          },
+          {
+            "name": "wasmer/memory-usage",
+            "value": 18732,
+            "unit": "kB",
+            "extra": "shim: 15412 kB\nzygote: 3320 kB"
+          },
+          {
+            "name": "wasmtime/memory-usage",
+            "value": 19656,
+            "unit": "kB",
+            "extra": "shim: 16340 kB\nzygote: 3316 kB"
           }
         ]
       }
